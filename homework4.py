8cffa4d06d7a8399140f8a3b5e5ce69502e41fed
OlegVasilevU <trade.vasilev@yandex.ru>
2024-07-01 18:57:58 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Организация программ и методы строк."
+# 2 Организация программы
+my_string = str(input('Введите произвольный текст: '))
+print(len(my_string))
+# Работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
